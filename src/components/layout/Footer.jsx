52bc0949104c5c4a5ef10f64881ecc2460bfb2e6
Gimhan Rodrigo <gimhanrg@gmail.com>
2024-05-04 08:05:38 +0530

    link changes

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,12 +5,12 @@ import {
   IconButton,
   Stack,
   Typography,
-} from '@mui/material';
+} from "@mui/material";
 
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import EmailIcon from '@mui/icons-material/Email';
-import { Link } from 'react-router-dom';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import EmailIcon from "@mui/icons-material/Email";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -20,28 +20,28 @@ const Footer = () => {
       pr={8}
       pb={3}
       sx={{
-        bgcolor: 'background.secondary',
+        bgcolor: "background.secondary",
       }}
     >
       <Stack
-        direction={{ xs: 'column', sm: 'row' }}
+        direction={{ xs: "column", sm: "row" }}
         spacing={{ xs: 2, sm: 3 }}
-        justifyContent='space-between'
-        alignItems='center'
+        justifyContent="space-between"
+        alignItems="center"
       >
         <Box>
-          <Typography variant='h4' align='center' color='text.primary'>
+          <Typography variant="h4" align="center" color="text.primary">
             Get in touch...
           </Typography>
         </Box>
-        <Box display='flex' flexDirection='column' alignItems='center'>
-          <Grid container spacing={4} justifyContent='center'>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Grid container spacing={4} justifyContent="center">
             <Grid item>
               <IconButton
-                size='medium'
-                target='_blank'
-                href='https://www.linkedin.com/in/gimhan-rodrigo-23a5961b9/'
-                color='primary'
+                size="medium"
+                target="_blank"
+                href="https://www.linkedin.com/in/gimhanrodrigo"
+                color="primary"
                 disableRipple
               >
                 <LinkedInIcon />
@@ -49,10 +49,10 @@ const Footer = () => {
             </Grid>
             <Grid item>
               <IconButton
-                size='medium'
-                target='_blank'
-                href='https://www.instagram.com/gimhanamandyana/'
-                color='primary'
+                size="medium"
+                target="_blank"
+                href="https://www.instagram.com/gimhanamandyana/"
+                color="primary"
                 disableRipple
               >
                 <InstagramIcon />
@@ -60,9 +60,9 @@ const Footer = () => {
             </Grid>
             <Grid item>
               <IconButton
-                size='medium'
-                href='mailto:gimhanr9@gmail.com'
-                color='primary'
+                size="medium"
+                href="mailto:gimhanr9@gmail.com"
+                color="primary"
                 //onClick={() => (window.location = 'mailto:yourmail@domain.com')}
 
                 disableRipple
@@ -72,18 +72,18 @@ const Footer = () => {
             </Grid>
           </Grid>
           <Box mt={1} />
-          <Typography variant='body2' align='center' color='text.primary'>
+          <Typography variant="body2" align="center" color="text.primary">
             OR
           </Typography>
           <Box mt={1} />
 
-          <Button component={Link} to='/message' variant='outlined'>
+          <Button component={Link} to="/message" variant="outlined">
             Send a message
           </Button>
         </Box>
       </Stack>
-      <Box display='flex' justifyContent='center' mt={3}>
-        <Typography variant='body2' color='text.primary' align='center'>
+      <Box display="flex" justifyContent="center" mt={3}>
+        <Typography variant="body2" color="text.primary" align="center">
           &copy; Gimhan Rodrigo. All rights reserved.
         </Typography>
       </Box>
